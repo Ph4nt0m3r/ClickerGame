0f5c4b49ca675e7ccd982e340222789d934dbc9f
Ph4nt0m3r <58989398+Ph4nt0m3r@users.noreply.github.com>
2020-01-04 18:56:15 +0300

    + Добавил 12 элементов

--- app/src/main/java/ru/phantomer/clicker/Element.java
+++ app/src/main/java/ru/phantomer/clicker/Element.java
@@ -26,7 +26,18 @@ public enum Element {
     BISMUTH("Висмут", 80, Color.WHITE, Color.parseColor("#8f8777"), "PRODUCE SOME BISMUTH!", "висмута"),
     PHOSPHORUS("Фосфор", 85, Color.WHITE, Color.parseColor("#910a15"), "PRODUCE SOME PHOSPHORUS!", "фосфора"),
     COBALT("Кобальт", 90, Color.BLACK, Color.parseColor("#ebedfc"), "PRODUCE SOME COBALT!", "кобальта"),
-    ;
+    PLATINUM("Платина",95,Color.BLACK,Color.parseColor("#e6f5ff"),"PRODUCE SOME PLATINUM!","платины"),
+    NICKEL("Никель",100,Color.BLACK,Color.parseColor("#f0f5df"),"PRODUCE SOME NICKEL!","никеля"),
+    Hydrogen("Водород",105,Color.BLACK, Color.WHITE,"PRODUCE SOME HYDROGEN!","водорода"),
+    Nitrogen("Азот",110,Color.BLACK,Color.WHITE,"PRODUCE SOME NITROGEN!","азота" ),
+    Oxygen ("Кислород", 115,Color.BLACK,Color.parseColor("#cffdff"),"PRODUCE SOME OXYGEN!","кислорода"),
+    Manganese ("Марганец",120,Color.BLACK,Color.parseColor("#e6eaff"),"PRODUCE SOME MANGANESE!","марганца"),
+    Chlorine ("Хлор",125, Color.BLACK,Color.parseColor("#c7ff66"),"PRODUCE SOME CHLORINE!","хлора"),
+    Barium ("Барий",130, Color.WHITE,Color.parseColor("#4d4f49"),"PRODUCE SOME BARIUM!", "бария"),
+    Molybdenum ("Молибден",135, Color.BLACK, Color.parseColor("#b5b8ae"),"PRODUCE SOME MOLYBDENUM!","молибдена"),
+    Tungsten ("Фольфрам",140, Color.BLACK,Color.parseColor("#ceedc5"),"PRODUCE SOME TUNGSTEN!", "вольфрама"),
+    Tellurium ("Теллур",145,Color.BLACK, Color.parseColor("#f5f1e4"), "PRODUCE SOME TELLURIUM!", "теллура"),
+    Uranium ("Уран", 150, Color.BLACK,Color.parseColor("#eafcac"),"PRODUCE SOME URANIUM!","урана"),;
 
     private String name;
     private int upgradeStatus;
