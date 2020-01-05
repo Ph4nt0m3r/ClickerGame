298257ddf574910849a0f7b7810ef341945376cf
Ph4nt0m3r <58989398+Ph4nt0m3r@users.noreply.github.com>
2020-01-05 23:58:33 +0300

    + Новые механики
    +Подтверждение сброса уровня, новая переменная для каждого элемента, оптимизицая кода.

--- app/src/main/java/ru/phantomer/clicker/Element.java
+++ app/src/main/java/ru/phantomer/clicker/Element.java
@@ -8,36 +8,36 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Element {
-    TREES("Дерево", 5, Color.WHITE, Color.parseColor("#357d2f"), "CHOP DOWN SOME TREES!", "дерева"),
-    STONE("Камень", 10, Color.WHITE, Color.parseColor("#999c98"), "MINE SOME STONE!", "камня"),
-    COAL("Уголь", 15, Color.WHITE, Color.parseColor("#4c4d4c"), "MINE SOME COAL!", "угля"),
-    COPPER("Медь", 20, Color.WHITE, Color.parseColor("#e87d13"), "MINE SOME COPPER!", "меди"),
-    SILVER("Серебро", 25, Color.BLACK, Color.parseColor("#ebebeb"), "MINE SOME SILVER!", "серебра"),
-    GOLD("Золото", 30, Color.BLACK, Color.parseColor("#fff93d"), "MINE SOME GOLD!", "золота"),
-    LEAD("Свинец", 35, Color.WHITE, Color.parseColor("#b1c0c9"), "MINE SOME LEAD!", "свинца"),
-    TIN("Олово", 40, Color.BLACK, Color.parseColor("#f7fff7"), "MINE SOME TIN!", "олова"),
-    IRON("Железо", 45, Color.BLACK, Color.parseColor("#dbdad5"), "MINE SOME IRON!", "железа"),
-    CARBON("Углерод", 50, Color.WHITE, Color.BLACK, "PRODUCE SOME CARBON!", "углерода"),
-    ANTIMONY("Сурьма", 55, Color.BLACK, Color.parseColor("#b6f4fa"), "PRODUCE SOME ANTIMONY!", "сурьмы"),
-    QUICKSILVER("Ртуть", 60, Color.BLACK, Color.parseColor("#d6d6d6"), "PRODUCE SOME QUICKSILVER!", "ртути"),
-    ZINC("Цинк", 65, Color.BLACK, Color.parseColor("#d7eafc"), "PRODUCE SOME ZINC!", "цинка"),
-    SULFUR("Сера", 70, Color.BLACK, Color.parseColor("#e6f725"), "PRODUCE SOME SULFUR!", "серы"),
-    ARSENIC("Мышьяк", 75, Color.BLACK, Color.parseColor("#e0ded1"), "PRODUCE SOME ARSENIC!", "мышьяка"),
-    BISMUTH("Висмут", 80, Color.WHITE, Color.parseColor("#8f8777"), "PRODUCE SOME BISMUTH!", "висмута"),
-    PHOSPHORUS("Фосфор", 85, Color.WHITE, Color.parseColor("#910a15"), "PRODUCE SOME PHOSPHORUS!", "фосфора"),
-    COBALT("Кобальт", 90, Color.BLACK, Color.parseColor("#ebedfc"), "PRODUCE SOME COBALT!", "кобальта"),
-    PLATINUM("Платина",95,Color.BLACK,Color.parseColor("#e6f5ff"),"PRODUCE SOME PLATINUM!","платины"),
-    NICKEL("Никель",100,Color.BLACK,Color.parseColor("#f0f5df"),"PRODUCE SOME NICKEL!","никеля"),
-    Hydrogen("Водород",105,Color.BLACK, Color.WHITE,"PRODUCE SOME HYDROGEN!","водорода"),
-    Nitrogen("Азот",110,Color.BLACK,Color.WHITE,"PRODUCE SOME NITROGEN!","азота" ),
-    Oxygen ("Кислород", 115,Color.BLACK,Color.parseColor("#cffdff"),"PRODUCE SOME OXYGEN!","кислорода"),
-    Manganese ("Марганец",120,Color.BLACK,Color.parseColor("#e6eaff"),"PRODUCE SOME MANGANESE!","марганца"),
-    Chlorine ("Хлор",125, Color.BLACK,Color.parseColor("#c7ff66"),"PRODUCE SOME CHLORINE!","хлора"),
-    Barium ("Барий",130, Color.WHITE,Color.parseColor("#4d4f49"),"PRODUCE SOME BARIUM!", "бария"),
-    Molybdenum ("Молибден",135, Color.BLACK, Color.parseColor("#b5b8ae"),"PRODUCE SOME MOLYBDENUM!","молибдена"),
-    Tungsten ("Фольфрам",140, Color.BLACK,Color.parseColor("#ceedc5"),"PRODUCE SOME TUNGSTEN!", "вольфрама"),
-    Tellurium ("Теллур",145,Color.BLACK, Color.parseColor("#f5f1e4"), "PRODUCE SOME TELLURIUM!", "теллура"),
-    Uranium ("Уран", 150, Color.BLACK,Color.parseColor("#eafcac"),"PRODUCE SOME URANIUM!","урана"),;
+    TREES("Дерево", 5, Color.WHITE, Color.parseColor("#357d2f"), "CHOP DOWN SOME TREES!", "дерева",0),
+    STONE("Камень", 10, Color.WHITE, Color.parseColor("#999c98"), "MINE SOME STONE!", "камня",0),
+    COAL("Уголь", 15, Color.WHITE, Color.parseColor("#4c4d4c"), "MINE SOME COAL!", "угля",0),
+    COPPER("Медь", 20, Color.WHITE, Color.parseColor("#e87d13"), "MINE SOME COPPER!", "меди",0),
+    SILVER("Серебро", 25, Color.BLACK, Color.parseColor("#ebebeb"), "MINE SOME SILVER!", "серебра",0),
+    GOLD("Золото", 30, Color.BLACK, Color.parseColor("#fff93d"), "MINE SOME GOLD!", "золота",0),
+    LEAD("Свинец", 35, Color.WHITE, Color.parseColor("#b1c0c9"), "MINE SOME LEAD!", "свинца",0),
+    TIN("Олово", 40, Color.BLACK, Color.parseColor("#f7fff7"), "MINE SOME TIN!", "олова",0),
+    IRON("Железо", 45, Color.BLACK, Color.parseColor("#dbdad5"), "MINE SOME IRON!", "железа",0),
+    CARBON("Углерод", 50, Color.WHITE, Color.BLACK, "PRODUCE SOME CARBON!", "углерода",0),
+    ANTIMONY("Сурьма", 55, Color.BLACK, Color.parseColor("#b6f4fa"), "PRODUCE SOME ANTIMONY!", "сурьмы",0),
+    QUICKSILVER("Ртуть", 60, Color.BLACK, Color.parseColor("#d6d6d6"), "PRODUCE SOME QUICKSILVER!", "ртути",0),
+    ZINC("Цинк", 65, Color.BLACK, Color.parseColor("#d7eafc"), "PRODUCE SOME ZINC!", "цинка",0),
+    SULFUR("Сера", 70, Color.BLACK, Color.parseColor("#e6f725"), "PRODUCE SOME SULFUR!", "серы",0),
+    ARSENIC("Мышьяк", 75, Color.BLACK, Color.parseColor("#e0ded1"), "PRODUCE SOME ARSENIC!", "мышьяка",0),
+    BISMUTH("Висмут", 80, Color.WHITE, Color.parseColor("#8f8777"), "PRODUCE SOME BISMUTH!", "висмута",0),
+    PHOSPHORUS("Фосфор", 85, Color.WHITE, Color.parseColor("#910a15"), "PRODUCE SOME PHOSPHORUS!", "фосфора",0),
+    COBALT("Кобальт", 90, Color.BLACK, Color.parseColor("#ebedfc"), "PRODUCE SOME COBALT!", "кобальта",0),
+    PLATINUM("Платина",95,Color.BLACK,Color.parseColor("#e6f5ff"),"PRODUCE SOME PLATINUM!","платины",0),
+    NICKEL("Никель",100,Color.BLACK,Color.parseColor("#f0f5df"),"PRODUCE SOME NICKEL!","никеля",0),
+    Hydrogen("Водород",105,Color.BLACK, Color.WHITE,"PRODUCE SOME HYDROGEN!","водорода",0),
+    Nitrogen("Азот",110,Color.BLACK,Color.WHITE,"PRODUCE SOME NITROGEN!","азота",0),
+    Oxygen ("Кислород", 115,Color.BLACK,Color.parseColor("#cffdff"),"PRODUCE SOME OXYGEN!","кислорода",0),
+    Manganese ("Марганец",120,Color.BLACK,Color.parseColor("#e6eaff"),"PRODUCE SOME MANGANESE!","марганца",0),
+    Chlorine ("Хлор",125, Color.BLACK,Color.parseColor("#c7ff66"),"PRODUCE SOME CHLORINE!","хлора",0),
+    Barium ("Барий",130, Color.WHITE,Color.parseColor("#4d4f49"),"PRODUCE SOME BARIUM!", "бария",0),
+    Molybdenum ("Молибден",135, Color.BLACK, Color.parseColor("#b5b8ae"),"PRODUCE SOME MOLYBDENUM!","молибдена",0),
+    Tungsten ("Фольфрам",140, Color.BLACK,Color.parseColor("#ceedc5"),"PRODUCE SOME TUNGSTEN!", "вольфрама",0),
+    Tellurium ("Теллур",145,Color.BLACK, Color.parseColor("#f5f1e4"), "PRODUCE SOME TELLURIUM!", "теллура",0),
+    Uranium ("Уран", 150, Color.BLACK,Color.parseColor("#eafcac"),"PRODUCE SOME URANIUM!","урана",0),;
 
     private String name;
     private int upgradeStatus;
@@ -45,4 +45,5 @@ public enum Element {
     private int backgroundColor;
     private String displayName;
     private String sklonenie;
+    public double elementAmount;
 }
--- app/src/main/java/ru/phantomer/clicker/MainActivity.java
+++ app/src/main/java/ru/phantomer/clicker/MainActivity.java
@@ -10,10 +10,10 @@ import android.content.pm.ActivityInfo;
 public class MainActivity extends AppCompatActivity {
 
     private TextView output, themeButton, prestigeButton, clickButton, upgradeButton;
-    private double itemAmount = 0;
     private int upgrade = 100, upgradeCounter = 0;
     private int addAmount = 1;
-    private int prestige = 0, needForPrestige = 5;
+    private int prestige = 0, needForPrestige = 10;
+   /*temp*/ private int prestigeClickAmount =0;
     private int clickingProducing = 1, upgradeProducing = 1;
     private boolean themeNow = false;
     private int white = 0xFFFFFFFF, black = 0xFF000000;
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         output = findViewById(R.id.textView);
-        output.setText("Кол-во дерева: " + itemAmount + "\n" + "PRESTIGE LVL= " + prestige + "\n" + "PRODUCING= " + clickingProducing + "\n" + "LVL= " + upgradeCounter);
+        output.setText("Кол-во дерева: " + 0 + "\n" + "PRESTIGE LVL= " + prestige + "\n" + "PRODUCING= " + clickingProducing + "\n" + "LVL= " + upgradeCounter);
         upgradeButton = findViewById(R.id.button2);
         upgradeButton.setText("UPGRADE!" + "\n" + "Цена апгрейда: " + "\n" + upgrade);
         clickButton = findViewById(R.id.button);
@@ -33,33 +33,38 @@ public class MainActivity extends AppCompatActivity {
         themeButton = findViewById(R.id.button5);
     }
 
+    //Клики и вывод текста на textView
+    public void clickHandler(View view) {
+        CurrentElement().elementAmount += clickingProducing;
+        output.setText("Кол-во " + CurrentElement().getSklonenie() + ": " + CurrentElement().elementAmount + "\n" + "PRESTIGE LVL= " + prestige + "\n" + "PRODUCING= " + clickingProducing + "\n" + "LVL= " + upgradeCounter);
+
+        /*temp*/ prestigeClickAmount = 0;
+
+    }
+
     //Улучшения и вывод текста на textView
     public void upgradeHandler(View view) {
-        getPresentElement();
-        if (itemAmount >= upgrade) {
-            itemAmount -= upgrade;
+        CurrentElement();//?
+        if (CurrentElement().elementAmount >= upgrade) {
+            CurrentElement().elementAmount -= upgrade;
             clickingProducing++;
             upgrade += 10 * upgradeProducing;
             upgradeCounter++;
             upgradeProducing++;
             upgradeButton.setText("UPGRADE!" + "\n" + "Цена апгрейда: " + "\n" + upgrade);
 
-            output.setText("Кол-во " + getPresentElement().getSklonenie() + ": " + itemAmount + "\n" + "PRESTIGE LVL= " + prestige + "\n" + "PRODUCING= " + clickingProducing + "\n" + "LVL= " + upgradeCounter);
+            output.setText("Кол-во " + CurrentElement().getSklonenie() + ": " + CurrentElement().elementAmount + "\n" + "PRESTIGE LVL= " + prestige + "\n" + "PRODUCING= " + clickingProducing + "\n" + "LVL= " + upgradeCounter);
         }
-    }
 
-    //Клики и вывод текста на textView
-    public void clickHandler(View view) {
-        itemAmount += clickingProducing;
-        output.setText("Кол-во " + getPresentElement().getSklonenie() + ": " + itemAmount + "\n" + "PRESTIGE LVL= " + prestige + "\n" + "PRODUCING= " + clickingProducing + "\n" + "LVL= " + upgradeCounter);
+        /*temp*/ prestigeClickAmount = 0;
+
     }
 
     //Определение ресурсов и их вывод на кнопку
-    public Element getPresentElement() {
+    public Element CurrentElement() {
 
         if ((upgradeCounter % 5 == 1 && addAmount == upgradeCounter / 5)
                 || (upgradeCounter == 5 && addAmount == 1)) {
-            itemAmount = 0;
             addAmount++;
         }
 
@@ -74,30 +79,28 @@ public class MainActivity extends AppCompatActivity {
         return Element.TREES;
     }
 
-    //TODO: + 30. + Сменить фон на картинку + сменить иконку + смена цвета текста +
-    // меню выбора цвета + подтверждение сброса уровня +
-    // придумать более увлекательную формулу (мб переменная для всех ресов?) +
-    // инвентарь и постройки?
-
-    /**
-     * Механика престижа.
-     *
-     * @param view - позволяет увидеть кнопке метод
-     */
+    // Механика престижа.
     public void prestigeReset(View view) {
-        if (upgradeCounter >= needForPrestige) {
+
+            /*temp*/if (prestigeClickAmount==1) {
+            /*temp*/ prestigeClickAmount++;
+            /*temp*/ prestigeButton.setText("Подтвердите действие повторным нажатием");
+            }
+            /*temp*/ else if ((prestigeClickAmount<=2) & (upgradeCounter < needForPrestige));
+            /*temp*/ prestigeButton.setText("PRESTIGE!" + "\n" + "REQ.LVL= " + needForPrestige);
+
+        if ((upgradeCounter >= needForPrestige)&(prestigeClickAmount == 2)) {
             prestige++;
-            itemAmount = 0;
             upgrade = 100;
             upgradeCounter = 0;
             addAmount = 1;
             upgradeProducing = 1;
-            output.setText("Кол-во " + getPresentElement().getSklonenie() + ": " + itemAmount + "\n" + "PRESTIGE LVL= " + prestige + "\n" + "PRODUCING= " + clickingProducing + "\n" + "LVL= " + upgradeCounter);
+            output.setText("Кол-во " + CurrentElement().getSklonenie() + ": " + CurrentElement().elementAmount + "\n" + "PRESTIGE LVL= " + prestige + "\n" + "PRODUCING= " + clickingProducing + "\n" + "LVL= " + upgradeCounter);
             upgradeButton.setText("Цена апгрейда: " + "\n" + upgrade);
-            clickButton.setText(getPresentElement().getDisplayName());
-            clickButton.setTextColor(getPresentElement().getTextColor());
-            clickButton.setBackgroundColor(getPresentElement().getBackgroundColor());
-            needForPrestige += 5;
+            clickButton.setText(CurrentElement().getDisplayName());
+            clickButton.setTextColor(CurrentElement().getTextColor());
+            clickButton.setBackgroundColor(CurrentElement().getBackgroundColor());
+            needForPrestige += 10;
             prestigeButton.setText("PRESTIGE!" + "\n" + "REQ.LVL= " + needForPrestige);
         }
     }
@@ -109,6 +112,15 @@ public class MainActivity extends AppCompatActivity {
         themeButton.setBackgroundColor(themeNow ? black : white);
         themeButton.setTextColor(themeNow ? white : black);
         themeNow = !themeNow;
+
+        /*temp*/ prestigeClickAmount = 0;
+
     }
+
+    //TODO: + 30. + Сменить фон на картинку + сменить иконку + смена цвета текста +
+    // меню выбора цвета + подтверждение сброса уровня (Диалоговое окно) +
+    // придумать более увлекательную формулу +
+    // инвентарь и постройки?
+
 }
 
